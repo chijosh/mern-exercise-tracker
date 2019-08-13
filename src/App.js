9a634699cedc9bcf6da1fb1aac0a91280a11802e
Admin <admin@Tietohallintos-Mac-mini.local>
2019-08-13 15:53:08 +0300

    frontend react router setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import NavBar from "./components/navbar.component";
+import ExerciseList from "./components/exercise-list.component";
+import EditExercise from "./components/edit-exercise.component"
+import CreateExercise from "./components/create-exercise.component"
+import CreateUser from "./components/create-user.component"
 
 function App() {
   return (
-    <div className="App">
-        <p>
-          Getting Started.
-        </p>
-    </div>
+    <Router>
+    <NavBar />
+    <br />
+    <Route path="/" exact component={ExerciseList} />
+    <Route path="/edit/:id" exact component={EditExercise} />
+    <Route path="/create" exact component={CreateExercise} />
+    <Route path="/user" exact component={CreateUser} />
+    </Router>
   );
 }
 
---
+++ src/components/create-user.component.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function createUser.component() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
